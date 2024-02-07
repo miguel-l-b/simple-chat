d8e7bef5986fe3783406f4e84531bb6a56e497d6
miguel-l-b <miguellopesbraido@gmail.com>
2024-02-07 14:39:51 -0300

    feat: font of emoji `twemoji`

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Home from './pages/Home'
+import twemoji from 'twemoji'
 
 
 export default function Router() {
+  useEffect(() => {
+    twemoji.parse(document.body, {
+      folder: 'svg',
+      ext: '.svg',
+    })
+  })
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<div>Not Found</div>} />
