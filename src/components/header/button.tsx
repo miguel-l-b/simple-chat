2e4be9023bede8b5e7767fc1665c3c1313b09b73
miguel-l-b <miguellopesbraido@gmail.com>
2024-02-08 22:24:18 -0300

    feat: allowing button scroller to element

--- src/components/header/button.tsx
+++ src/components/header/button.tsx
@@ -1,14 +1,34 @@
-import { Link } from "react-router-dom";
+import { RefObject } from "react"
+import { Link } from "react-router-dom"
 
-export interface HeaderButtonProps extends React.HTMLAttributes<HTMLAnchorElement> {
-  to: string;
+export interface HeaderButtonProps extends React.HTMLAttributes<HTMLElement> {
+  to: string | RefObject<any>;
   children: React.ReactNode;
 }
 
 export default function Button({ to, children, ...props }: HeaderButtonProps) {
+  if(typeof to === "string")
+    return (
+      <Link
+        to={to}
+        className={`
+          text-md font-semibold px-3 py-1.5
+          border-b-4 border-blue-900 text-gray-900
+          transition-all duration-500 hover:text-blue-900 hover:py-1 hover:border-blue-400
+        `}
+        {...props}
+      >
+        {children}
+      </Link>
+    )
+
   return (
-    <Link
-      to={to}
+    <button
+      onClick={() => {
+        if(to.current) {
+          to.current.scrollIntoView({ behavior: "smooth" })
+        }
+      }}
       className={`
         text-md font-semibold px-3 py-1.5
         border-b-4 border-blue-900 text-gray-900
@@ -17,6 +37,8 @@ export default function Button({ to, children, ...props }: HeaderButtonProps) {
       {...props}
     >
       {children}
-    </Link>
+    </button>
   )
+
+
 }
