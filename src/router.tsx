7f5d5b040de838ed24a9e20b71b27680bb3bdb97
miguel.braido <miguel.braido@pecaai.com.br>
2024-12-06 07:48:21 -0300

    feat: add Analytics

--- src/router.tsx
+++ src/router.tsx
@@ -1,39 +1,42 @@
-import { useEffect, useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Home from './pages/Home'
-import twemoji from 'twemoji'
-import Chat from './pages/Chat'
-import PopUp from './components/pop_up'
-import Profile from './pages/Chat/Profile'
-
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import twemoji from "twemoji";
+import Chat from "./pages/Chat";
+import PopUp from "./components/pop_up";
+import Profile from "./pages/Chat/Profile";
 
 export default function Router() {
   twemoji.parse(document.body, {
-    folder: 'svg',
-    ext: '.svg',
-  })
-  
-  const [alertDemo, setAlertDemo] = useState(localStorage.getItem("alert-demo"))
+    folder: "svg",
+    ext: ".svg",
+  });
+
+  const [alertDemo, setAlertDemo] = useState(
+    localStorage.getItem("alert-demo")
+  );
 
   useEffect(() => {
-      localStorage.setItem("alert-demo", alertDemo || "false")
-  }, [alertDemo])
+    localStorage.setItem("alert-demo", alertDemo || "false");
+  }, [alertDemo]);
 
   return (
     <>
-      {alertDemo === "false" &&
+      {alertDemo === "false" && (
         <PopUp
           type="warning"
           title="Bem vindo ao SimpleChat"
           message="Este site é uma demo de um cliente, portanto não coloque informações pessoais nem utilize para conversar com outras pessoas, isso é um DEMO!"
           button={{
             type: "ok",
-            onClick: () => setAlertDemo("true")
+            onClick: () => setAlertDemo("true"),
           }}
         />
-      }
+      )}
+      <Analytics />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -46,5 +49,5 @@ export default function Router() {
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
